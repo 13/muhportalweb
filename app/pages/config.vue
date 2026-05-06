79e9b0ae81c4d3330aa140f6612383b7cdb97782
13 <13@users.noreply.github.com>
2026-05-06 12:27:10 +0200

    updated about page

--- app/pages/config.vue
+++ app/pages/config.vue
@@ -16,8 +16,7 @@
   </v-app-bar>
 
   <v-container fluid class="d-flex justify-center pa-0 pa-md-6">
-    <div class="d-flex flex-column ga-4 w-100" style="max-width: 800px;">
-
+    <div class="d-flex flex-column ga-4 w-100" style="max-width: 800px">
       <v-card class="ma-0">
         <v-list>
           <v-list-item>
@@ -36,64 +35,41 @@
         </v-list>
       </v-card>
 
-      <v-card class="ma-0">
-        <v-card-title class="text-h6">About</v-card-title>
-
+      <v-card class="ma-0 text-center">
         <v-card-text>
-          <div class="d-flex flex-column flex-sm-row align-center ga-4">
+          <div class="d-flex flex-column align-center ga-4">
+            <!-- Logo -->
             <v-avatar size="88" rounded="lg" class="bg-white elevation-2">
               <v-img src="/muhportal.svg" alt="muhportalweb logo" />
             </v-avatar>
 
-            <div class="flex-grow-1 text-center text-sm-left">
-              <div class="text-h5 font-weight-bold">muhportalweb</div>
-              <div class="text-body-1 text-medium-emphasis mt-2">
-                MUH Portal Web is a lightweight home automation frontend for portal access,
-                wake-on-lan and Home Assistant controls.
-              </div>
+            <!-- Name -->
+            <div class="text-h5 font-weight-bold">muhportalweb</div>
+
+            <!-- Description -->
+            <div class="text-body-1 text-medium-emphasis">
+              MUH Portal Web is a lightweight home automation frontend for
+              portal access, wake-on-lan and Home Assistant controls.
             </div>
           </div>
         </v-card-text>
 
-        <v-divider />
-
-        <v-list density="comfortable">
-          <v-list-item>
-            <template #prepend>
-              <v-icon>mdi-tag</v-icon>
-            </template>
-            <v-list-item-title>Version</v-list-item-title>
-            <v-list-item-subtitle>{{ appVersion }}</v-list-item-subtitle>
-          </v-list-item>
-
-          <v-list-item>
-            <template #prepend>
-              <v-icon>mdi-rocket-launch</v-icon>
-            </template>
-            <v-list-item-title>Release</v-list-item-title>
-            <v-list-item-subtitle>Stable client build</v-list-item-subtitle>
-          </v-list-item>
-
-          <v-list-item>
-            <template #prepend>
-              <v-icon>mdi-calendar-clock</v-icon>
-            </template>
-            <v-list-item-title>Build Date</v-list-item-title>
-            <v-list-item-subtitle>{{ formattedBuildDate }}</v-list-item-subtitle>
-          </v-list-item>
-
-        </v-list>
+        <!-- Version + Build -->
+        <v-card-text class="pt-0 text-center">
+          <div class="text-body-2">Version: {{ appVersion }}</div>
+          <div class="text-body-2">Build Date: {{ formattedBuildDate }}</div>
+        </v-card-text>
       </v-card>
     </div>
   </v-container>
 </template>
 
 <script setup lang="ts">
-import pkg from '../../package.json'
+import pkg from "../../package.json";
 
-const appVersion = pkg.version
-const buildDate = useRuntimeConfig().public.buildDate as string
-const formattedBuildDate = new Date(buildDate).toLocaleString()
+const appVersion = pkg.version;
+const buildDate = useRuntimeConfig().public.buildDate as string;
+const formattedBuildDate = new Date(buildDate).toLocaleString();
 const isDarkModeEnabled = ref(false);
 const setDarkMode = inject<(enableDarkMode: boolean) => void>("setDarkMode");
 
